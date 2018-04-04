06464bde54426640557df9d33998d4e8b8e4dd3a
Jessica <jessica@Jessicas-MacBook-Pro.local>
2018-04-04 15:31:24 -0600

    refactor(CardFooter): Separate CardFooter into it's own component

---
+++ components/CardFooter.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faFileAlt } from '@fortawesome/fontawesome-free-regular';
+
+const StyledFooter = styled.div`
+  align-items: center;
+  display: grid;
+  font-size: 10px;
+  grid-template-columns: .3fr 2fr 1fr;
+  grid-template-rows: 20px;
+  color: darkgrey;
+  padding-left: 10px;
+  padding-right: 10px;
+
+  .date {
+    justify-self: end;
+  }
+
+  .icon {
+    padding-bottom: 3px;
+  }
+`;
+
+export default function CardFooter(props) {
+  return (
+    <StyledFooter>
+      <div className="icon">
+        <FontAwesomeIcon icon={faFileAlt} size="2x" />
+      </div>
+      <div>
+        {props.readtime}
+      </div>
+      <div className="date">{props.date}</div>
+    </StyledFooter>
+  );
+}
+
+CardFooter.propTypes = {
+  date: PropTypes.string.isRequired,
+  readtime: PropTypes.string.isRequired,
+};

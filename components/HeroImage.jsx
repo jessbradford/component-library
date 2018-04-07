9b2ab22c5089f643b789fc089fbdb691293ceb54
Jessica <jessica@Jessicas-MacBook-Pro.local>
2018-04-07 13:05:38 -0600

    feat(HeroImage): Create Initial HeroImage component
    #13

---
+++ components/HeroImage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: black;
+`;
+
+export default function HeroImage() {
+  return (
+    <StyledDiv />
+  );
+}

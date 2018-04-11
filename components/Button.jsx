4d6e636476cc32ca2914ae05bab0806916104670
Jessica Bradford <32757215+jessbradford@users.noreply.github.com>
2018-04-11 13:07:20 -0600

    19 create button (#24)
    * feat(Button): Add button
    
    #19
    
    * style(Button): Style button
    
    #19

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Colors from '../constants/Colors';
 
-export default function Button({ onClick, children }) {
-  return (<button onClick={onClick}>{children}</button>);
+const StyledButton = styled.button`
+  background-color: ${Colors.blue} ;
+  border: ${Colors.blue};
+  border-radius: 5px;
+  color: white;
+  font-size: 14px;
+  padding: 10px 25px;
+  -webkit-transition-duration: 0.4s;
+  transition-duration: 0.4s;
+  &:hover {
+    opacity: 0.9;
+  }
+`;
+
+export default function Button(props) {
+  return (
+    <StyledButton className="button" onClick={props.onClick}>
+      {props.buttonName}
+    </StyledButton>
+  );
 }
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  buttonName: PropTypes.string.isRequired,
+};

1efd2c4e28fac62f37b5a6946bef5b3e5f7c3cf7
Jessica <jessica@Jessicas-MacBook-Pro.local>
2018-04-16 09:58:34 -0600

    feat(index.js): Export components
    #30

---
+++ index.js
@@ -0,0 +1,25 @@
+import Button from './components/Button';
+import Card from './components/Card';
+import CardDescription from './components/CardDescription';
+import CardFooter from './components/CardFooter';
+import CardImage from './components/CardImage';
+import CardOutline from './components/CardOutline';
+import CardTitle from './components/CardTitle';
+import Divider from './components/Divider';
+import Somnium from './components/Somnium';
+import StarIcon from './components/StarIcon';
+import Stars from './components/Stars';
+
+export {
+  Button,
+  Card,
+  CardDescription,
+  CardFooter,
+  CardImage,
+  CardOutline,
+  CardTitle,
+  Divider,
+  Somnium,
+  StarIcon,
+  Stars,
+};
